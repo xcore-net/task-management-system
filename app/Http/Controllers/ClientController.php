87c5d0ae774d22689d8b33c63d750e57687016ac
odai Borasheed <odaiten@gmail.com>
2024-05-26 01:25:00 +0300

    modified controller logic & routes; installed vite

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Client;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        $clients = Client::all();
+        return view('client-manager.client-manager', ['clients' => $clients]);
+    }
+
+    public function store(Request $request)
+    {
+        $client = new Client();
+        $client->name = $request->name;
+        $client->email = $request->email;
+        $client->phone = $request->phone;
+        $client->save();
+        return response('user created.', 201);
+    }
+
+    public function destory($id)
+    {
+        $client = Client::find($id);
+        $client->delete();
+        return redirect('/client')
+            ->setStatusCode(204)
+            ->with('status', 'user deleted.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/client', [ClientController::class, 'index'])->name('client.index');
+    Route::post('/client', [ClientController::class, 'store'])->name('client.store');
+    Route::delete('/client/{id}', [ClientController::class, 'destory'])->name('client.destory');
+});
+
 
 require __DIR__.'/auth.php';
