b2d917def6e750a1451bd91381dd7e77b6b4dc82
odai Borasheed <odaiten@gmail.com>
2024-05-26 11:14:40 +0300

    added styles for client view

--- resources/views/client-manager/client-manager.blade.php
+++ resources/views/client-manager/client-manager.blade.php
@@ -1,4 +1,31 @@
-<form action="/client" method="POST">
+<header>
+   <style>
+    .client-creator-form{
+        width: fit-content;
+        max-width: 50%;
+        padding: 1em;
+        border: 1px solid #000;
+    }
+.form-group{
+    padding: 0.5em;
+    display:flex;
+    justify-content: space-around;
+    gap: 0.4em
+}
+
+.client-creator-btn{
+    padding: 1em;
+    width:100%
+}
+
+tr>*{
+    padding: 1em;
+    margin-left: 2px;
+}
+tr::{color:lightblue}
+   </style>
+</header>
+<form class="client-creator-form" action="/client" method="POST">
     @csrf
     <div class="form-group">
         <label for="name">Name:</label>
@@ -12,7 +39,7 @@
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
     </div>
-    <button type="submit">Create Client</button>
+    <button class="client-creator-btn" type="submit">Create Client</button>
 </form>
 
 <table>
