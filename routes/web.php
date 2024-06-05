1430d2220f0a56e2acea8612e9d2af4018acf09b
Saleem_Abou_Assi <amhadi696@gmail.com>
2024-06-05 11:01:33 +0300

    give admin and user permisions

--- routes/web.php
+++ routes/web.php
@@ -23,23 +23,18 @@ Route::get('/dashboard', function () {
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    //client
-    Route::get('/client', [ClientController::class, 'index'])->name('client.index');
-    Route::get('/client/create', [ClientController::class, 'create'])->name('client.create');
-    Route::post('/client/store', [ClientController::class, 'store'])->name('client.store');
-    Route::get('/client/{id}', [ClientController::class, 'show'])->name('client.show');
-    Route::get('/client/{id}/edit', [ClientController::class, 'edit'])->name('client.edit');
-    Route::put('/client/{id}', [ClientController::class, 'update'])->name('client.update');
+Route::middleware('role:admin')->group(function () {
     Route::delete('/client/{id}', [ClientController::class, 'destroy'])->name('client.destroy');
-
-    //form
+    Route::delete('/form/{id}', [FormController::class, 'destroy'])->name('form.destroy');
+});
+Route::middleware('role:admin|user')->group(function () {
+     //form
     Route::get('/form', [FormController::class, 'index'])->name('form.index');
     Route::get('/form/create', [FormController::class, 'create'])->name('form.create');
     Route::post('/form/store', [FormController::class, 'store'])->name('form.store');
     Route::get('/form/{id}', [FormController::class, 'show'])->name('form.show');
     Route::get('/form/{id}/edit', [FormController::class, 'edit'])->name('form.edit');
     Route::put('/form/{id}', [FormController::class, 'update'])->name('form.update');
-    Route::delete('/form/{id}', [FormController::class, 'destroy'])->name('form.destroy');
 
     //field
     Route::get('/field', [FieldController::class, 'index'])->name('field.index');
@@ -49,6 +44,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/field/{id}/edit', [FieldController::class, 'edit'])->name('field.edit');
     Route::put('/field/{id}', [FieldController::class, 'update'])->name('field.update');
     Route::delete('/field/{id}', [FieldController::class, 'destroy'])->name('field.destroy');
+    
+});
+    //client
+    Route::get('/client', [ClientController::class, 'index'])->name('client.index');
+    Route::get('/client/create', [ClientController::class, 'create'])->name('client.create');
+    Route::post('/client/store', [ClientController::class, 'store'])->name('client.store');
+    Route::get('/client/{id}', [ClientController::class, 'show'])->name('client.show');
+    Route::get('/client/{id}/edit', [ClientController::class, 'edit'])->name('client.edit');
+    Route::put('/client/{id}', [ClientController::class, 'update'])->name('client.update');
+    
+
+   
+    
+
+    
 
     //Doc types
     Route::get('/documentType', [DocumentTypeController::class, 'index'])->name('documentType.index');
