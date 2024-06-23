df3e1eec2ad5488bfa777f708d603cb3abb40391
odai Borasheed <odaiten@gmail.com>
2024-06-23 10:00:44 +0300

    added forgetten routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ApiDocumentRequestController;
 use App\Http\Controllers\Api\ApiFormController;
 use App\Http\Controllers\Api\ApiTaskController;
 use App\Http\Controllers\Api\ApiTaskTypeController;
 use App\Http\Controllers\Api\ApiUploadedFilesController;
-
+use App\Http\Controllers\ApiDocumentTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -33,9 +34,23 @@ Route::get('/taskType/{id}', [ApiTaskTypeController::class, 'show'])->name('task
 Route::put('/taskType/{id}', [ApiTaskTypeController::class, 'update'])->name('taskType.update');
 Route::delete('/taskType/{id}', [ApiTaskTypeController::class, 'destroy'])->name('taskType.destroy');
 
- //Task
- Route::get('/task', [ApiTaskController::class, 'index'])->name('task.index');
- Route::post('/task/store', [ApiTaskController::class, 'store'])->name('task.store');
- Route::get('/task/{id}', [ApiTaskController::class, 'show'])->name('task.show');
- Route::put('/task/{id}', [ApiTaskController::class, 'update'])->name('task.update');
- Route::delete('/task/{id}', [ApiTaskController::class, 'destroy'])->name('task.destroy');
+//Task
+Route::get('/task', [ApiTaskController::class, 'index'])->name('task.index');
+Route::post('/task/store', [ApiTaskController::class, 'store'])->name('task.store');
+Route::get('/task/{id}', [ApiTaskController::class, 'show'])->name('task.show');
+Route::put('/task/{id}', [ApiTaskController::class, 'update'])->name('task.update');
+Route::delete('/task/{id}', [ApiTaskController::class, 'destroy'])->name('task.destroy');
+
+//Doc request
+Route::get('/document_request', [ApiDocumentRequestController::class, 'index'])->name('document_request.index');
+Route::post('/document_request/store', [ApiDocumentRequestController::class, 'store'])->name('document_request.store');
+Route::get('/document_request/{id}', [ApiDocumentRequestController::class, 'show'])->name('document_request.show');
+Route::put('/document_request/{id}', [ApiDocumentRequestController::class, 'update'])->name('document_request.update');
+Route::delete('/document_request/{id}', [ApiDocumentRequestController::class, 'destroy'])->name('document_request.destroy');
+
+//Doc types
+Route::get('/documentType', [ApiDocumentTypeController::class, 'index'])->name('documentType.index');
+Route::post('/documentType/store', [ApiDocumentTypeController::class, 'store'])->name('documentType.store');
+Route::get('/documentType/{id}', [ApiDocumentTypeController::class, 'show'])->name('documentType.show');
+Route::put('/documentType/{id}', [ApiDocumentTypeController::class, 'update'])->name('documentType.update');
+Route::delete('/documentType/{id}', [ApiDocumentTypeController::class, 'destroy'])->name('documentType.destroy');
